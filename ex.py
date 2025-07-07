79c86fb0a3847140de9545bed3a28d2fc6ac2d9b
kiwoo38 <kimki41233@gmail.com>
2025-07-08 02:58:15 +0900

    password

--- ex.py
+++ ex.py
@@ -1,18 +1,27 @@
 import random
+random.seed()
+s="abcdefghijklmnopqrstuvwxyz"
+base_s=[]
 
-namelist=[]
-oxlist=[]
+base_s=random.sample(s,26)
+password=[]
+password_hint=list(zip(s,base_s))
+print(password_hint)
+str=input("사용자 입력: ")
 
-count=int(input("인원을 입력하세요: "))
+for letter in str:
+    for i in range(26):
+        if letter==password_hint[i][0]:
+            password.append(password_hint[i][1])
 
-for i in range(count):
-    name=input("이름을 입력하세요: ")
-    namelist.append(name)
-for i in range(count):
-    oxlist.append(random.choice(["o","x"]))
+print(password)
+result=""
 
-tname=tuple(namelist)
-print(tname)
-
-t=list(zip(tname,oxlist))
-print(t)
+input_str2=input("암호화된 문자열 입력: ")
+ 
+for letter2 in input_str2:
+    for i in range(26):
+        if letter2==password_hint[i][1]:
+            result+=password_hint[i][0]
+            break
+print(result)
